f79eed212b7f4423eb6e192245f059f78ffeaa65
Daniel Oliveira <danielso2007@gmail.com>
2018-11-09 14:16:25 -0200

    feat(webpack): Iniciando estudo de webpack

---
+++ exercicios_webpack/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './ex/index.js',
+    mode: 'development',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        rules: [{
+            test: /.js?$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['transform-object-rest-spread']
+                },
+            }
+        }]
+    }
+};
