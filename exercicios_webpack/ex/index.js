107732af78f241640426b049ab4473463d442630
Daniel Oliveira <danielso2007@gmail.com>
2018-11-09 14:47:01 -0200

    feat(webpack): Adicionando o Preset do React.

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,15 +1,5 @@
-const produto = {
-    nome: 'Caneta Bic Preta',
-    preco: 1.90,
-    desconto: 0.05
-}
+import 'react'
 
+export default props => ( <h1>Olá</h1> )
 
-function clone(object) {
-    return {...object}
-}
-
-const novoProduto = clone(produto);
-novoProduto.nome = 'Caneta Bic Azul';
-
-console.log(produto, novoProduto);
+console.log('Funcionou!');
