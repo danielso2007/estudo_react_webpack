992d792cc8131949fa439a75cd697760e3ff2348
Daniel Oliveira <danielso2007@gmail.com>
2018-11-12 13:20:26 -0200

    feat: Componentes Controlados/Não Controlados.

--- exercicios_react/ex.8/classComponent.jsx
+++ exercicios_react/ex.8/classComponent.jsx
@@ -11,12 +11,12 @@ export default class ClassComponent extends Component {
   }
   render() {
     return (
-    <div>
-        <h1>{this.props.label}</h1>
-        <h2>{this.state.value}</h2>
-        <button onClick={() => this.sum(-1)}>Dec</button>
-        <button onClick={() => this.sum(1)}>Inc</button>
-    </div>
+      <div>
+          <h1>{this.props.label}</h1>
+          <h2>{this.state.value}</h2>
+          <button onClick={() => this.sum(-1)}>Dec</button>
+          <button onClick={() => this.sum(1)}>Inc</button>
+      </div>
     );
   }
 }
