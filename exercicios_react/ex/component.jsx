3032344a8a04ab8984a60d2f6bfd06145b774842
Daniel Oliveira <danielso2007@gmail.com>
2018-11-12 12:02:43 -0200

    feat: Mais de um componente no react.

--- exercicios_react/ex/component.jsx
+++ exercicios_react/ex/component.jsx
@@ -1,3 +1,5 @@
 import React from "react";
 
-export default props => <h1>{props.value}</h1>;
+export default props => <h1>Primeiro Componente!</h1>;
+
+export const Segundo = props => <h1>Segundo Componente!</h1>;
