8c428f88fa14961c5964c0bc2387fce16043fc7d
Daniel Oliveira <danielso2007@gmail.com>
2018-11-22 15:19:13 -0200

    feat: FInalizado estudo react com redux.

--- exercicios_react/ex/counterActions.js
+++ exercicios_react/ex/counterActions.js
@@ -1,14 +1,14 @@
 export function inc() {
-  return { type: 'INC' };
+    return { type: 'INC' }
 }
 
 export function dec() {
-  return { type: 'DEC' };
+    return { type: 'DEC' }
 }
 
 export function stepChanged(e) {
-  return {
-    type: 'STEP_CHANGED',
-    payload: e.target.value,
-  };
+    return {
+        type: 'STEP_CHANGED',
+        payload: e.target.value
+    }
 }
