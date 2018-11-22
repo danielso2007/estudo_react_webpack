62ee06a0d52e7d738a4937a4945bfb2f9fa699c1
Daniel Oliveira <danielso2007@gmail.com>
2018-11-22 15:13:50 -0200

    feat: Instalado o eslint.

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Field from "./field";
-import { combineReducers, createStore } from "redux";
-import { Provider } from "react-redux";
-import fieldReducer from "./fieldReducer";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import counterReducer from './counterReducer';
+import Counter from './counter';
 
 const reducers = combineReducers({
-  field: fieldReducer
+  counter: counterReducer
 });
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
-    <Field inicialValue="Teste" />
+    <Counter />
   </Provider>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
