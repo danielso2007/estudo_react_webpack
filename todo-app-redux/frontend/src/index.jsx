da8822a14a51b01973f6a93bfd742dd393c04161
Daniel Oliveira <danielso2007@gmail.com>
2018-11-22 16:35:37 -0200

    feat: Aplicando Middleware (Redux-Promise).

--- todo-app-redux/frontend/src/index.jsx
+++ todo-app-redux/frontend/src/index.jsx
@@ -4,9 +4,10 @@ import App from "./main/app";
 import { HashRouter } from "react-router-dom";
 import reducers from "./main/reducers";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
 
-const store = createStore(reducers);
+const store = applyMiddleware(promise)(createStore)(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
