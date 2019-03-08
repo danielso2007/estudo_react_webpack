dda56a6f701db9d9fe81261e56692b1b75d4257a
Daniel Oliveira <danielso2007@gmail.com>
2019-03-08 20:26:49 -0300

    feat: Obtendo a Qtde de Registros (Count).

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -4,4 +4,14 @@ BillingCycle.methods(['get', 'post', 'put', 'delete']);
 // Ajustes e obrigatoriedades no PUT - Update
 BillingCycle.updateOptions({new: true, runValidators: true});
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+            res.status(500).json({errors: [error]});
+        } else {
+            res.json({value});
+        }
+    });
+});
+
 module.exports = BillingCycle;
