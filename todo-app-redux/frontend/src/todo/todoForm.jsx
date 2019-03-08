abcf4e20f9b7e5e0c232de7565b3a711f6ffd672
Daniel Oliveira <danielso2007@gmail.com>
2019-03-08 13:29:41 -0300

    feat: Melhorando a Action de Pesquisa.

--- todo-app-redux/frontend/src/todo/todoForm.jsx
+++ todo-app-redux/frontend/src/todo/todoForm.jsx
@@ -14,7 +14,7 @@ class TodoForm extends Component {
   keyHandler(e) {
     const { add, search, description } = this.props;
     if (e.key === 'Enter') {
-      e.shiftKey ? search() : add(description)
+      e.shiftKey ? search(description) : add(description)
     } else if (e.key === 'Escape') {
       this.props.clear();
     }
@@ -41,7 +41,7 @@ class TodoForm extends Component {
         </Grid>
         <Grid cols="12 5 3">
           <IconButton style="primary" icon="plus" onClick={ () => add(description) } />
-          <IconButton style="info" icon="search" onClick={ () => search(description) } />
+          <IconButton style="info" icon="search" onClick={ search } />
           <IconButton
             style="default"
             icon="close"
